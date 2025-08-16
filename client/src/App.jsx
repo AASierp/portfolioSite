6ba97e92b374e99ff5835c3c0cbd4b0d3ede72a2
Aaron Sierp <aaronsierp@gmail.com>
2025-08-16 16:23:28 -0400

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+import Index from './pages';
+import './css/styles.css';
+
+function App() {
+  return(
+  <Index />
+  )
+}
+
+export default App
---
+++ client/src/pages/Index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Index() {
+  return (
+    <div className="main-container">
+      <nav>
+        <a href="#about">About Me</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+      <div className="title">
+        <h1>I'm Aaron Sierp</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Index;
